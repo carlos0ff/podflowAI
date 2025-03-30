79a532d2c51db1983b0537f7e8233a6625d5d60a
carlos0ff <carlosiilva66@gmail.com>
2025-03-30 19:16:09 -0300

    Inicializando projeto Spring Boot e Angular

---
+++ backend/src/main/java/com/backend/podflowai/PodflowaiApplication.java
@@ -0,0 +1,14 @@
+package com.backend.podflowai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodflowaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodflowaiApplication.class, args);
+	}
+
+}
+gi
---
+++ backend/src/test/java/com/backend/podflowai/PodflowaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.podflowai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodflowaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
